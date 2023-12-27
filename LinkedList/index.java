64b7d38f0109daed0f853630bffae60cb14afbbc
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-12-27 23:15:31 +0530

    Add files via upload

---
+++ LinkedList/index.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+class LL{
+    private Node head ;
+    private Node tail;
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node (int value , Node next){
+            this.value = value ;
+            this.next = next;
+        }
+    }
+}
+
+public class index {
+    public static void main(String[] args) {
+        
+    }
+}
